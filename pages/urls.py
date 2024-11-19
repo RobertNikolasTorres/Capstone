27bba170488a9ec0be47cc287b2141b9110b4f18
robert <robert.nikolas.torres@gmail.com>
2024-11-19 07:22:29 -0800

    assignment1

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from .views import home, about, contact
+
+urlpatterns = [
+    path('', home, name='root'),
+    path('home/', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+
+]
